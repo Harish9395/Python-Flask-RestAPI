daa121bd416d5929ebce553e5fa33262ee178596
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-01-30 18:42:05 +0530

    Update karate-config.js

--- karate-api-tests/karate-config.js
+++ karate-api-tests/karate-config.js
@@ -1,10 +1,19 @@
 function fn() {
     var config = {};
+
     config.baseUrl = java.lang.System.getenv('API_BASE_URL');
     if (!config.baseUrl) {
         karate.log('❌ API_BASE_URL not set!');
         throw 'API_BASE_URL environment variable is required';
     }
+
     config.authToken = java.lang.System.getenv('AUTH_TOKEN') || '';
+
+    karate.configure('report', {
+        showLog: true,
+        showAllSteps: true,
+        showKarateVersion: true
+    });
+
     return config;
 }
