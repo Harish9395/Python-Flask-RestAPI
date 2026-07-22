7507ede168b0cde60aa62121190d0695154c96bd
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-23 03:32:41 +0530

    Create test_response_headers.py

---
+++ tests/regression/test_response_headers.py
@@ -0,0 +1,8 @@
+def test_security_headers(page, base_url):
+    response = page.goto(base_url)
+    headers = response.headers
+    assert "content-type" in headers
+    assert (
+        headers["content-type"]
+        .startswith("text/html")
+    )
