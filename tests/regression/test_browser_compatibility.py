1fc4a0471b9ea9a59e9f5c5501b1a6cc0e7517b3
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-23 04:03:33 +0530

    Update test_browser_compatibility.py

--- tests/regression/test_browser_compatibility.py
+++ tests/regression/test_browser_compatibility.py
@@ -1,12 +1,24 @@
 import pytest
 
 
-def test_browser_launch(browser, base_url):
-    context = browser.new_context()
-    page = context.new_page()
+@pytest.mark.parametrize(
+    "browser_name",
+    [
+        "chromium",
+        "firefox",
+        "webkit"
+    ]
+)
+def test_browser_launch(browser_name, playwright, base_url):
+
+    browser_type = getattr(playwright, browser_name)
+
+    browser = browser_type.launch()
+
+    page = browser.new_page()
 
     response = page.goto(base_url)
 
     assert response.ok
 
-    context.close()
+    browser.close()
