f22289c022a7bbf373a1e141cefb32f06662887a
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-23 15:51:17 +0530

    Update test_response_headers.py

--- tests/regression/test_response_headers.py
+++ tests/regression/test_response_headers.py
@@ -1,8 +1,11 @@
+import pytest
+
+@pytest.mark.regression
 def test_security_headers(page, base_url):
+
     response = page.goto(base_url)
+
     headers = response.headers
+
     assert "content-type" in headers
-    assert (
-        headers["content-type"]
-        .startswith("text/html")
-    )
+    assert headers["content-type"].startswith("text/html")
