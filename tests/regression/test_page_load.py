8563c4c714e1ce07a94c84b6557297ba18698978
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-23 15:50:42 +0530

    Update test_page_load.py

--- tests/regression/test_page_load.py
+++ tests/regression/test_page_load.py
@@ -1,4 +1,9 @@
+import pytest
+
+@pytest.mark.regression
 def test_application_loads(page, base_url):
+
     response = page.goto(base_url)
+
     assert response is not None
     assert response.status == 200
