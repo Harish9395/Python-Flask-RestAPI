2655786efe5c838d01e27a444fa3bcd86c82e224
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-23 15:54:12 +0530

    Update test_ui_elements.py

--- tests/regression/test_ui_elements.py
+++ tests/regression/test_ui_elements.py
@@ -1,3 +1,6 @@
+import pytest
+
+@pytest.mark.regression
 def test_home_page_content(page, base_url):
 
     page.goto(base_url)
@@ -6,7 +9,7 @@ def test_home_page_content(page, base_url):
 
     assert body.is_visible()
 
-    assert (
-        "App Works!!!"
-        in body.text_content()
-    )
+    page_content = body.text_content()
+
+    assert page_content is not None
+    assert "App Works!!!" in page_content
