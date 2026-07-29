834612b27d6026b3c9c8116fecf4323cc5a97fe3
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-30 02:48:43 +0530

    Update app_service.py

--- app_service.py
+++ app_service.py
@@ -1,51 +1,76 @@
-import json
-
 class AppService:
-    
-    tasks = [
-        {
-            'id': 1,
-            'name': "task1",
-            "description": "This is task 1"
-        },
-        {
-            "id": 2,
-            "name": "task2",
-            "description": "This is task 2"
-        },
-        {
-            "id": 3,
-            "name": "task3",
-            "description": "This is task 3"
-        }
-    ]
 
     def __init__(self):
-        self.tasksJSON = json.dumps(self.tasks)
+
+        self.tasks = [
+            {
+                "id": 1,
+                "name": "task1",
+                "description": "This is task 1"
+            },
+            {
+                "id": 2,
+                "name": "task2",
+                "description": "This is task 2"
+            },
+            {
+                "id": 3,
+                "name": "task3",
+                "description": "This is task 3"
+            }
+        ]
+
 
     def get_tasks(self):
-        return self.tasksJSON
 
-    def create_task(self,task):
-        tasksData = json.loads(self.tasksJSON)
-        tasksData.append(task)
-        self.tasksJSON = json.dumps(tasksData)
-        return self.tasksJSON
+        return self.tasks
+
+
+    def create_task(self, task):
+
+        new_id = len(self.tasks) + 1
+
+        new_task = {
+            "id": new_id,
+            "name": task,
+            "description": f"This is {task}"
+        }
+
+        self.tasks.append(new_task)
+
+        return new_task
+
 
     def update_task(self, request_task):
-        tasksData = json.loads(self.tasksJSON)
-        for task in tasksData:
-            if task["id"] == request_task['id']:
+
+        for task in self.tasks:
+
+            if task["id"] == request_task["id"]:
+
                 task.update(request_task)
-                return json.dumps(tasksData);
-        return json.dumps({'message': 'task id not found'});
+
+                return task
+
+
+        return {
+            "message": "task id not found"
+        }
+
 
     def delete_task(self, request_task_id):
-        tasksData = json.loads(self.tasksJSON)
-        for task in tasksData:
-            if task["id"] == request_task_id:
-                tasksData.remove(task)
-                return json.dumps(tasksData);
-        return json.dumps({'message': 'task id not found'});
 
-    
+        for task in self.tasks:
+
+            if task["id"] == request_task_id:
+
+                self.tasks.remove(task)
+
+                return {
+                    "message": "task deleted",
+                    "tasks": self.tasks
+                }
+
+
+        return {
+            "message": "task id not found"
+        }
